3f387c2fc26689a9e92f2e952f5f2c555d21aef5
okaysong <carver.song@gmail.com>
2026-04-07 17:09:50 +0800

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -4,8 +4,8 @@ import datetime
 
 # 替换为你之前拼接好的真实 RSS 链接
 FEEDS = {
-    "🎬 电影": "https://rsshub.app/imdb/user/你的ID/ratings",
-    "📚 阅读": "https://www.goodreads.com/review/list_rss/你的ID?shelf=read",
+    "🎬 电影": "https://rsshub.app/imdb/list/ls001577067",
+    "📚 阅读": "https://www.goodreads.com/review/list_rss/2055861?key=iyISFZZeSgNPyClqkXkYc75_ToCciDCuQjs4WB2on-6Va1U7&shelf=read",
     "🎵 音乐": "https://ws.audioscrobbler.com/2.0/user/你的ID/recenttracks.rss",
     "📍 足迹": "https://feeds.foursquare.com/history/你的ID.rss?signature=你的密钥"
 }
